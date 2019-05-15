1d49354a8afd479379c74a7280d423c133975deb
Nemanja <nemanjavuk99@gmail.com>
2019-05-16 00:52:28 +0200

    Updated index.php, and created checkError function

--- app/includes/functions.php
+++ app/includes/functions.php
@@ -1,7 +1,21 @@
 <?php
 // Checks if the given url exists, if it does, means that the YT channel name is valid
-function checkURL($url){
+function checkURL($url)
+{
         
     $headers = get_headers($url);
     return stripos($headers[0],"200 OK") ? true : false;
+}
+
+// Used for setting the errors when sending data to front
+function checkError($setError, $setErrorCode, $setData = null)
+{
+     // response variables
+    global $error;
+    global $errorCode;
+    global $data;
+
+    $error = $setError;
+    $errorCode = $setErrorCode;
+    $data = $setData;
 }
